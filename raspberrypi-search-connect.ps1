abe854c6a7f7f3ff8ae760204c35afcefa3da4ff
YA <YA-androidapp@users.noreply.github.com>
2021-08-22 10:39:41 +0900

    Update raspberrypi-search-connect.ps1

--- raspberrypi-search-connect.ps1
+++ raspberrypi-search-connect.ps1
@@ -20,3 +20,9 @@ arp -a | Select-String "dc-a6-32-" | ForEach-Object {
         start ssh "pi@$raspiipaddr"
     }
 }
+arp -a | Select-String "e4-5f-01-" | ForEach-Object {
+    [RegEx]::Matches($_, "[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+") | ForEach-Object {
+        $raspiipaddr = $_.Value
+        start ssh "pi@$raspiipaddr"
+    }
+}
